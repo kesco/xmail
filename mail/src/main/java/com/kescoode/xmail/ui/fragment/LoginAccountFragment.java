2f2f297c77b1082232d7ea8ae54247f7319f662e
Kesco Lin <kesco915@gmail.com>
2015-04-08 10:57:21 +0800

    修改登录页面UI

--- mail/src/main/java/com/kescoode/xmail/ui/fragment/LoginAccountFragment.java
+++ mail/src/main/java/com/kescoode/xmail/ui/fragment/LoginAccountFragment.java
@@ -1,15 +1,17 @@
 package com.kescoode.xmail.ui.fragment;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.RemoteException;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.EditText;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import com.dd.CircularProgressButton;
 import com.kescoode.adk.log.Logger;
 import com.kescoode.xmail.R;
 import com.kescoode.xmail.event.SettingCheckEvent;
@@ -31,6 +33,9 @@ public class LoginAccountFragment extends AppFragment<AccountActivity> {
     @InjectView(R.id.et_email_passwd)
     EditText etPasswd;
 
+    @InjectView(R.id.cpb_login)
+    CircularProgressButton cpbLogin;
+
     private final EventBus bus = EventBus.getDefault();
 
     public static LoginAccountFragment newInstance() {
@@ -44,6 +49,7 @@ public class LoginAccountFragment extends AppFragment<AccountActivity> {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -52,6 +58,13 @@ public class LoginAccountFragment extends AppFragment<AccountActivity> {
         View view = inflater.inflate(R.layout.fragment_login_account, container, false);
         ButterKnife.inject(this, view);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getAct().getSupportActionBar().setTitle(R.string.login_label);
+        cpbLogin.setIndeterminateProgressMode(true);
     }
 
     @Override
@@ -70,8 +83,9 @@ public class LoginAccountFragment extends AppFragment<AccountActivity> {
         }
     }
 
-    @OnClick(R.id.btn_login)
+    @OnClick(R.id.cpb_login)
     void btnLoginClick() {
+        cpbLogin.setProgress(50);
         try {
             if (getAct().mailService != null) {
                 getAct().mailService.login(etEmail.getText().toString().trim(),
@@ -82,12 +96,20 @@ public class LoginAccountFragment extends AppFragment<AccountActivity> {
         }
     }
 
-    public void onEvent(SettingCheckEvent event) {
+    public void onEventMainThread(SettingCheckEvent event) {
         Logger.e("Login Event: %s %s", String.valueOf(event.ok), event.type.name());
-        if (event.type == SettingCheckEvent.Type.RECEIVE) {
-            Intent intent = new Intent(getAct(), HomeActivity.class);
-            startActivity(intent);
-            getAct().finish();
+        if (event.type == SettingCheckEvent.Type.RECEIVE && event.ok) {
+            cpbLogin.setProgress(100);
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getAct(), HomeActivity.class);
+                    startActivity(intent);
+                    getAct().finish();
+                }
+            }, 1500);
+        } else if (!event.ok) {
+            cpbLogin.setProgress(-1);
         }
     }
 
@@ -97,4 +119,19 @@ public class LoginAccountFragment extends AppFragment<AccountActivity> {
         ButterKnife.reset(this);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_login_actions, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                // TODO: 加入App说明
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
